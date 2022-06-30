0588da248e4c442c273f7ef570ca90fef03c88b0
Raven <theravengrey@gmail.com>
2022-07-01 00:55:42 +0200

    Destroy session if the session is invalid

--- app/routes/dashboard/index.tsx
+++ app/routes/dashboard/index.tsx
@@ -1,8 +1,26 @@
-import type { LoaderFunction } from "@remix-run/node";
+import type { ActionFunction, LoaderFunction} from "@remix-run/node";
+import { redirect } from "@remix-run/node";
 import { json } from "@remix-run/node";
+import { useFetcher } from "@remix-run/react";
 import { useEffect } from "react";
 import { SDK } from "~/appwrite";
-import { checkSession } from "~/session";
+import { checkSession, destroySession, getSession } from "~/session";
+
+export const action: ActionFunction = async ({ request }) => {
+	const body = await request.formData();
+  const invalidate = body.get('invalidate');
+  const session = await getSession(request.headers.get('Cookie'));
+  
+  if (invalidate === 'true') {
+    throw redirect("/auth", {
+      headers: {
+        "Set-Cookie": await destroySession(session),
+      },
+    });
+  }
+
+  return null
+};
 
 export const loader: LoaderFunction = async ({ request }) => {
   await checkSession(request);
@@ -12,13 +30,20 @@ export const loader: LoaderFunction = async ({ request }) => {
 
 const Dashboard = () => {
   // const data = useLoaderData();
+  const fetcher = useFetcher();
 
   useEffect(() => {
-    setTimeout(async () => {
-      console.log("asd")
-      await SDK.account.deleteSession('current')
-    }, 3000)
-  }, [])
+    (async() => {
+      try {
+        await SDK.account.get();
+      } catch (err) {
+        fetcher.submit(
+          { invalidate: 'true' },
+          { method: 'post' }
+        )
+      }
+    })();
+  }, [fetcher])
 
   return <p>ad</p>
 }
