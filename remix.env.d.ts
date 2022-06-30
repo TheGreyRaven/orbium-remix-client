e0f1b5ac927484e06ba943dbe9a5fc22e166f954
Raven <theravengrey@gmail.com>
2022-06-30 20:46:38 +0200

    Define env types

--- remix.env.d.ts
+++ remix.env.d.ts
@@ -1,2 +1,17 @@
 /// <reference types="@remix-run/dev" />
 /// <reference types="@remix-run/node/globals" />
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      APPWRITE_API: string;
+      APPWRITE_DOMAIN: string;
+      APPWRITE_ID: string;
+      SENDGRID_KEY: string;
+      NODE_ENV: 'development' | 'production';
+      PORT?: string;
+      PWD: string;
+    }
+  }
+}
+
+export {}
