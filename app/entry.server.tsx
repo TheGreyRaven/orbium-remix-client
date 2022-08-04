69fd509a5112166be16fd82808894ac1fb949eb9
Raven <theravengrey@gmail.com>
2022-08-04 23:55:36 +0200

    Added sitemap.xml, robots.txt

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -2,13 +2,18 @@ import { renderToString } from 'react-dom/server';
 import { RemixServer } from '@remix-run/react';
 import type { EntryContext } from '@remix-run/node';
 import { injectStylesIntoStaticMarkup } from '@mantine/ssr';
+import { otherRootRouteHandlers } from './routes.server';
 
-export default function handleRequest(
+export default async function handleRequest(
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
   remixContext: EntryContext
 ) {
+  for (const handler of otherRootRouteHandlers) {
+    const otherRouteResponse = await handler(request, remixContext);
+    if (otherRouteResponse) return otherRouteResponse;
+  }
   const markup = renderToString(<RemixServer context={remixContext} url={request.url} />);
   responseHeaders.set('Content-Type', 'text/html');
 
