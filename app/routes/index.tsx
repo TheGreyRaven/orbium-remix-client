2ee28d6894ed19659dc5cdf6d78f564f4b805e63
Raven <theravengrey@gmail.com>
2022-08-04 18:19:02 +0200

    Trigger deployment

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -94,7 +94,6 @@ const Index = () => {
           <List.Item>Write and publish news to your clients.</List.Item>
         </List>
         <TextAnchor text='See full list of' linkText='features' to='/features/' />
-
         <Button mt={12} size='xl' className={classes.startButton} component={Link} to='/auth/'>GET STARTED</Button>
         <TextAnchor text='By joining, you agree with our' linkText='Terms of Service' to='/tos/' />
       </Group>
